7caaae2576416d08e6129068bedc59ea87a2b3b7
Michael Stowe <stowem1@michigan.gov>
2024-09-30 16:08:22 -0400

    Initial file structure setup

---
+++ src/app/features/artist-profile/artist-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './artist-profile.component.html',
+  styleUrl: './artist-profile.component.scss'
+})
+export class ArtistProfileComponent {
+
+}
