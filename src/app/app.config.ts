ae82c8063f7b5daf6fad420f4de12abe39cef3cb
Michael Stowe <michaelstowe12@gmail.com>
2024-10-24 23:31:11 -0400

    Setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideNgIconsConfig } from '@ng-icons/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideNgIconsConfig({
+      size: '1.5em',
+    }),
+  ],
 };
--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, NgIconsModule, provideIcons } from '@ng-icons/core';
+import { jamBell, jamHeart } from '@ng-icons/jam-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIconComponent
+  ],
+  providers: [provideIcons({ jamHeart, jamBell })],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
